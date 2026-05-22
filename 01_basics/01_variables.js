35c923ee97e17139eeb96b33e84c47574625b0c0
arindamm04 <sarkararindam2004@gmail.com>
2026-05-22 12:12:37 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "sarkararindam2004@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState
+
+//accountId = 2//not allowed
+
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+//alert(3 + 3) // we are using nodejs, not browser
+
+// code readability should be high
+
+let name = "arindam"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof null); //object
